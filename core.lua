36e627fed6c26716b1262d317df59555b54f2505
Jonathan Simpson <jsimpson.github@gmail.com>
2018-08-19 09:44:51 -0700

    clean up

--- core.lua
+++ core.lua
@@ -52,58 +52,58 @@ duration:SetDrawLayer('OVERLAY')
 -- event handler
 local active = false
 local OnEventHandler = function(self, event)
-	if event == 'PLAYER_REGEN_DISABLED' then
-		f:SetAlpha(1)
-	elseif event == 'PLAYER_REGEN_ENABLED' then
-		f:SetAlpha(alpha)
-	elseif event == 'COMBAT_LOG_EVENT_UNFILTERED' then
-		local _, message, _, sourceGUID, _, _, _, _, _, _, _, spellId = CombatLogGetCurrentEventInfo()
-		if sourceGUID == UnitGUID('PLAYER') and spellId ~= nil and spellId == inquisitionId then
-			if message == 'SPELL_AURA_REMOVED' then
-				active = false
+  if event == 'PLAYER_REGEN_DISABLED' then
+    f:SetAlpha(1)
+  elseif event == 'PLAYER_REGEN_ENABLED' then
+    f:SetAlpha(alpha)
+  elseif event == 'COMBAT_LOG_EVENT_UNFILTERED' then
+    local _, message, _, sourceGUID, _, _, _, _, _, _, _, spellId = CombatLogGetCurrentEventInfo()
+    if sourceGUID == UnitGUID('PLAYER') and spellId ~= nil and spellId == inquisitionId then
+      if message == 'SPELL_AURA_REMOVED' then
+        active = false
 
-				f.icon:SetTexture(tex)
-				duration:SetText()
-			elseif message == 'SPELL_AURA_APPLIED' then
-				active = true
+        f.icon:SetTexture(tex)
+        duration:SetText()
+      elseif message == 'SPELL_AURA_APPLIED' then
+        active = true
 
-				f.icon:SetTexture(inquisitionTex)
-				duration:SetFormattedText("%d", remainingTime())
-			elseif message == 'SPELL_AURA_REFRESH' then
-				active = true
+        f.icon:SetTexture(inquisitionTex)
+        duration:SetFormattedText("%d", remainingTime())
+      elseif message == 'SPELL_AURA_REFRESH' then
+        active = true
 
-				duration:SetFormattedText("%d", remainingTime())
-			end
-		end
-	end
+        duration:SetFormattedText("%d", remainingTime())
+      end
+    end
+  end
 end
 
 -- update handler
 local timeSinceLastUpdate = 0
 local OnUpdate = function(self, elapsed)
-	timeSinceLastUpdate = timeSinceLastUpdate + elapsed
-	if timeSinceLastUpdate > 1.0 then
-		timeSinceLastUpdate = 0
+  timeSinceLastUpdate = timeSinceLastUpdate + elapsed
+  if timeSinceLastUpdate > 1.0 then
+    timeSinceLastUpdate = 0
 
-		if active then
-			duration:SetFormattedText("%d", remainingTime())
-		end
-	end
+    if active then
+      duration:SetFormattedText("%d", remainingTime())
+    end
+  end
 end
 
 -- remaining time on the buff
 function remainingTime()
-	for i=1,40 do
-		local name, _, _, _, _, expirationTime, _, _, _, spellId = UnitBuff('PLAYER', i)
-		if spellId == inquisitionId then
-			if expirationTime then
-				return expirationTime - GetTime()
-			else
-				return 0
-			end
-		end
-	end
-	return 0
+  for i=1,40 do
+    local name, _, _, _, _, expirationTime, _, _, _, spellId = UnitBuff('PLAYER', i)
+    if spellId == inquisitionId then
+      if expirationTime then
+        return expirationTime - GetTime()
+      else
+        return 0
+      end
+    end
+  end
+  return 0
 end
 
 -- connect the event and update handlers
